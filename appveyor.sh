5cec3ea3896a0f380c3b6f50e21d9ef147b90bdb
Ken Brown <kbrown@cornell.edu>
2017-03-29 11:26:16 -0400

    Tweak appveyor.sh

--- appveyor.sh
+++ appveyor.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 rm -f /usr/lib/libgnutls.la
 
-if cygport mailutils.cygport fetch prep compile
+exit_status=0
+
+cygport mailutils.cygport fetch prep compile || exit_status=1
+
+if [ $exit_status -eq 0 ]
 then
-    cygport mailutils.cygport test inst pkg
+    cygport mailutils.cygport test inst pkg || exit_status=1
 fi
 
 tar -cJf artifact.tar.xz mailutils-*/dist mailutils-*/log
+
+exit $exit_status
