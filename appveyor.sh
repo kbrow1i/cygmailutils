4bcd4b17f0c900039f635aabb3d17b1a95353597
Ken Brown <kbrown@cornell.edu>
2017-03-29 11:59:00 -0400

    Try (again) to replace missing readmsg tests

--- appveyor.sh
+++ appveyor.sh
@@ -7,7 +7,8 @@ cygport mailutils.cygport fetch prep compile || exit_status=1
 
 if [ $exit_status -eq 0 ]
 then
-    cygport mailutils.cygport test inst pkg || exit_status=1
+    cygport mailutils.cygport test
+    cygport mailutils.cygport inst pkg || exit_status=1
 fi
 
 tar -cJf artifact.tar.xz mailutils-*/dist mailutils-*/log
